9b9ad73e73477cea4858bf9e585fabe7f6f5d939
Weijia XU <weijia.xu0212@gmail.com>
2018-11-27 13:17:24 -0800

    change the function Try, which will return the number of pieces reversed as well.

--- chess.c
+++ chess.c
@@ -18,6 +18,8 @@ similarly, in Y direction, the position change is dy[8]
 */
 // chessboard
 int G[8][8];
+int ch_iter;//last change's number
+int cur_val;//current value
 
 typedef struct 
 {
@@ -46,9 +48,10 @@ void Place(int f, int x, int y) //f is the color, x,y is the location
 	}
 }
 
-bool Try(int f, int x, int y) //to chanek if G[x][y] can put a piece.
+int Try(int f, int x, int y, int change) //to chanek if G[x][y] can put a piece, also return pieces reversed.
 {
 	int nx, ny;
+	int ok = 0;
 	for(int i = 0;i < 8;i++)
 	{
 		nx = x + dx[i] + dx[i]
@@ -57,9 +60,28 @@ bool Try(int f, int x, int y) //to chanek if G[x][y] can put a piece.
 		{
 			if(G[nx][ny] == f && G[x + dx[i]][y + dy[i]] == -f)//a reverse should be applied
 			{
-				return true
+				ok++;
+				if(change)
+				{
+					G[x+dx[i]][y+dy[i]] = f;
+					cur_val += f + f;
+					changes[ch_iter] = (Change){x + dx[i], y  + dy[i], -f, f};
+					ch_iter++;
+				}
 			}
 		}
 	}
-	return false
+	if(ok)
+	{
+		if(change)
+		{
+			G[x][y] = f;
+			cur_val += f;
+			changes[ch_iter] = (Change){x, y, 0, f};
+			ch_iter++;
+		}
+		return ok;
+	}
+	else
+		return 0;
 }
